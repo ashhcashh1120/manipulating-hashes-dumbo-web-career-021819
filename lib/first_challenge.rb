15870a55639263249d89e06781034315039a62d1
Ashley Archibald <aarchibald20@gmail.com>
2019-02-15 14:16:52 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,12 @@ def first_challenge
 
   #your code here
 
-
+  contacts.each do|person, data|
+    data.each do |attr, value|
+      if attr == :favorite_icecream_flavors
+        value.delete_if {|flavor| flavor == "strawberry"}
+      end
+    end
   #remember to return your newly altered contacts hash!
   contacts
 end
